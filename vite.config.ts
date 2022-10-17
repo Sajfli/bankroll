81fd67f86436b080b30bf1122eb528653d11cb2c
Sajfli <sajmon88@protonmail.ch>
2022-10-17 09:13:40 +0200

    moved cert locations to .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,37 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { readFileSync } from 'fs'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react(), eslint()],
-    resolve: {
-        alias: {
-            '@': '/src',
-        },
-    },
-    server: {
-        host: 'localhost',
-        https: {
-            key: readFileSync('/home/zerowo/.cert/localhost-key.pem'),
-            cert: readFileSync('/home/zerowo/localhost.pem'),
-        },
-        proxy: {
-            '/api/v1': {
-                target: 'https://localhost:8443',
-                changeOrigin: false,
-                secure: false,
+const config = ({ mode }) => {
+    const env = loadEnv(mode, process.cwd())
+
+    return defineConfig({
+        plugins: [react(), eslint()],
+        resolve: {
+            alias: {
+                '@': '/src',
             },
         },
-    },
-    build: {
-        assetsDir: 'static',
-    },
-})
+        server: {
+            host: 'localhost',
+            https: {
+                key: readFileSync(env.VITE_CERT_KEY_LOCATION),
+                cert: readFileSync(env.VITE_CERT_LOCATION),
+            },
+            proxy: {
+                '/api/v1': {
+                    target: 'https://localhost:8443',
+                    changeOrigin: false,
+                    secure: false,
+                },
+            },
+        },
+        build: {
+            assetsDir: 'static',
+        },
+    })
+}
+
+export default config
