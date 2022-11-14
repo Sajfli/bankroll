b97565d32ed5d3996fdf0273daa1a1b525e2d567
Sajfli <sajmon88@protonmail.ch>
2022-11-14 16:57:40 +0100

    Add lazy loading to iframe

--- src/modules/atoms/IFrame.tsx
+++ src/modules/atoms/IFrame.tsx
@@ -1,13 +1,14 @@
 const IFrame = ({ src }: { src: string }) => {
     return (
         <iframe
-            src={`https://youtube.com/embed/${src}&origin=https://szymonjez.pl`}
+            src={`https://youtube.com/embed/${src}`}
             title="Film na youtube"
             width="560"
             height="315"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
+            loading="lazy"
         ></iframe>
     )
 }
